5610c513ef2bbe9908f973504e1fb325faf1dec8
AlexandreMPDias <alexandrempdias@icloud.com>
2021-03-08 13:40:56 -0300

    v0.0.4

--- src/domain/models/config-transformations.ts
+++ src/domain/models/config-transformations.ts
@@ -1,7 +1,7 @@
 import { Config } from '~cli/domain/models/configs';
 
-export type ConfigTransformations = Record<string, ConfigTransformations.Fn>;
+export type ConfigTransformations<K extends string = string> = Record<K, ConfigTransformations.Fn>;
 
 export namespace ConfigTransformations {
-	export type Fn = (partialConfig?: Partial<Config>) => Partial<Config>;
+	export type Fn = (partialConfig: Partial<Config>) => Partial<Config>;
 }
